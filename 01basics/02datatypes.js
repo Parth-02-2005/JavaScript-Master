42815f9e69600a72b86e068524fe3cd6856c8796
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-07-03 21:12:49 +0530

    datatypes

---
+++ 01basics/02datatypes.js
@@ -0,0 +1,25 @@
+'use strict'; // treat all JS code as newer version, new syntax
+// alert('hello') // we are using node.js not browser
+
+console.log(5 + 5);
+let name = 'parth gandhi' // string 
+let age = 21 // number
+let isLoggedIn = true // boolean
+
+// types of datatypes: Primitive datatypes 
+
+// 1. number
+// 2. string
+// 3. boolean  => true, false
+// 4. null => stand alone value // it is a representation of an empty value // it is an object type
+// 5. undefined => when variable is decelared but value is not assigned.
+// 6. symbol => to find unique ness
+
+// types of datatypes: Non-Primitive datatypes 
+
+// 1. object {} => key value pair
+
+console.log(typeof null); // type object
+console.log(typeof undefined); // type undefined
+
+
