8e76f03626a73e5d7368723ae180139c7ad59999
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-07-03 23:27:18 +0530

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,15 @@
+// primitive
+// 7 types: String, Number, Booolean, null, undefined, Symbol, BigInt
+// 
+// Non-primitive
+//  Arrays, Objects, Functions
+
+const heros = ['shaktiman', 'naagraj', 'doga']
+let myObj = {
+     name: 'parth gandhi',
+    age: 21
+}
+   
+console.log(typeof(heros));
+
+
