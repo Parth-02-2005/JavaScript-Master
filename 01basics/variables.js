d3eb84c1e06134136031f194bd64581669a6f597
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-07-03 20:53:36 +0530

    let const var

---
+++ 01basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 12334456 // once the vaiable is decelared it cannot be changed
+let accountEmail = 'parthgandhi@gmail.com' // can redeclare
+var accountPassword = '12345'
+accountCity = 'vadodara' // can allocate memnory but not recommended
+let accountState; // undefined
+console.log(accountState);
