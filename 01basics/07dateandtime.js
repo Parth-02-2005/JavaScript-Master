b72a49a5b12ae9f2ea0e3e6bc19d1f7155c84669
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-07-04 20:58:40 +0530

    all about date and time

---
+++ 01basics/07dateandtime.js
@@ -0,0 +1,10 @@
+// Dates
+
+const date = new Date() // instance of date
+console.log(date.getDay())
+console.log(date.toString());
+console.log(date.toLocaleDateString());
+
+let timeStamp = Date.now()
+console.log(timeStamp);
+
