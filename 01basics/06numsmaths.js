902eb75df819e43a8d5d0b2c826c1b274b097075
GANDHI PARTH MAHENDRA <gandhiparth.02.2005@gmail.com>
2025-07-04 20:41:13 +0530

    maths in js

---
+++ 01basics/06numsmaths.js
@@ -0,0 +1,14 @@
+// const num = 45
+// const num2 = 43434
+// const num3 = 1
+// let ans = Math.min(num, num2, num3);
+// console.log(ans);
+// abs => neg value converted to positive
+// round => 4.3 to 4 
+// ceil => select top number 
+// floor => select lowest value 
+// min, max
+// console.log(Math.floor(Math.random() * 5 + 1));
+
+
+
